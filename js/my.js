86f0b67a26a3a679689b9eebb4ed893e156052a0
Merviiiin <mervincoronas219@gmail.com>
2021-03-10 22:40:37 +0800

    PA-3

--- js/my.js
+++ js/my.js
@@ -1,75 +1,50 @@
-var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October","November", "December"];
-var day = [32, 29, 32, 31, 32, 31, 31, 32, 31, 32, 31, 32];
-var title = document.getElementById("title");
-window.onload = function(){    
-    var months = prompt("Please enter month [1-12]",);
-    var week = prompt("Please enter week [1-7]",);
+function Form(){
+    var subtotal, tax, total, amountpaid, change, percent, pay;
+    percent = 12;
+    pay = document.getElementById("pay")
+    subtotal = document.getElementById("subtotal").value;
+    tax = Number((percent/100) * subtotal);
+    total = Number(subtotal)+ Number(tax);
+    amountpaid = Number(total);
+    change = Number(amountpaid-total);
 
-    if(months >= 13 || week >= 8){
-        title.innerHTML = "Invalid Date";
+    if(subtotal > 99){
+        document.getElementById("tax").value = tax.toFixed(2);
+        document.getElementById("total").value = total.toFixed(2);
+        document.getElementById("amountpaid").value = amountpaid.toFixed(2);
+        document.getElementById("amountpaid").disabled = false;
+        document.getElementById("amountpaid").min = total.toFixed(2);
+        document.getElementById("change").value = change.toFixed(2);
+        pay.style.backgroundColor = "red";
+        pay.disabled = false;
     }
     else{
-
-        title.innerHTML = month[months-1];
-        display(months, week);
+        document.getElementById("tax").value = "";
+        document.getElementById("total").value = "";
+        document.getElementById("amountpaid").value = "";
+        document.getElementById("amountpaid").disabled = true;
+        document.getElementById("amountpaid").min = "";
+        document.getElementById("change").value = "";
+        pay.style.backgroundColor = "red";
+        pay.disabled = true;
     }
-    
-}
+};
+function changevalue(){
+    var subtotal, tax, total, amountpaid, change;
+    subtotal = document.getElementById("subtotal").value;
+    tax = document.getElementById("tax").value;
+    total =  document.getElementById("total").value;
+    amountpaid = document.getElementById("amountpaid").value;
+    change = Number(amountpaid-total);
 
-var table = document.getElementById("days");
-function display(months, week){
-    var row; var col = 0;
-    
-    for(i = 1; i < day[months-1]; i++){
-        if(i == 1){           
-             if(week == 1){
-                row = table.insertRow(-1);
-            }
-            else if(week == 2){
-                row = table.insertRow(-1);
-                row.insertCell(col++);                
-            }
-            else if(week == 3){
-                row = table.insertRow(-1);
-                row.insertCell(col++); 
-                row.insertCell(col++);
-            }
-            else if(week == 4){
-                row = table.insertRow(-1);
-                row.insertCell(col++); 
-                row.insertCell(col++);
-                row.insertCell(col++);
-            }
-            else if(week == 5){
-                row = table.insertRow(-1);
-                row.insertCell(col++); 
-                row.insertCell(col++);
-                row.insertCell(col++);
-                row.insertCell(col++);
-            }
-            else if(week == 6){
-                row = table.insertRow(-1);
-                row.insertCell(col++); 
-                row.insertCell(col++);
-                row.insertCell(col++);
-                row.insertCell(col++);
-                row.insertCell(col++);
-            }
-            else if(week == 7){
-                row = table.insertRow(-1);
-                row.insertCell(col++); 
-                row.insertCell(col++);
-                row.insertCell(col++);
-                row.insertCell(col++);
-                row.insertCell(col++);
-                row.insertCell(col++);
-            }    
+    document.getElementById("change").value = change.toFixed(2);
+
+        if(change > "-1"){
+            pay.style.backgroundColor = "#5de95d";
+            pay.disabled = false;
         }
-        if(col == 7){
-            row = table.insertRow(-1);
-            col = 0;
-        }    
-        val = row.insertCell(col++);
-        val.innerHTML = i;        
-    }
-}
+        else{
+            pay.style.backgroundColor = "#ff0000";
+            pay.disabled = true;
+        }
+};
